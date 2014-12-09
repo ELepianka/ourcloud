3123caafa56ec34325511a985539197b1a93f35f
ELepianka <errol.lepianka@uky.edu>
2014-12-09 15:54:52 -0500

    client connects! it doesn't read buf

--- echoclient.c
+++ echoclient.c
@@ -4,27 +4,42 @@
 /* $begin echoclientmain */
 #include "csapp.h"
 
-int main(int argc, char **argv) 
-{
-    int clientfd, port;
-    char *host, buf[MAXLINE];
+int main(int argc, char **argv) {
+
+    int clientfd, port, size;
+    //char* port;
+    char *host;
+    char buf[100*1024];
+    char command_type;
     rio_t rio;
 
-    if (argc != 3) {
-	fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+    if (argc < 5) {
+	fprintf(stderr, "usage: %s <command> <host> <port> <secret> optional<filename>\n", argv[0]);
 	exit(0);
     }
-    host = argv[1];
-    port = atoi(argv[2]);
+    host = argv[2];
+    port = atoi(argv[3]);
+    command_type = argv[1];
 
-    clientfd = Open_clientfd(host, port);
-    Rio_readinitb(&rio, clientfd);
+    clientfd = Open_clientfd(host, port); //creates a socket, creates an address, 
+					  //and establishes conenction
+    Rio_readinitb(&rio, clientfd);	  //initializes rio
 
-    while (Fgets(buf, MAXLINE, stdin) != NULL) {
-	Rio_writen(clientfd, buf, strlen(buf));
-	Rio_readlineb(&rio, buf, MAXLINE);
-	Fputs(buf, stdout);
+    if(command_type == 'put'){
+	size = fread(buf,sizeof(char),100*1024,stdin);
+	buf[size] = 0;
+	size += 1;
     }
+
+    Rio_writen(clientfd, buf, size);
+    Rio_readn(clientfd, buf, size);
+
+
+//    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+//	Rio_writen(clientfd, buf, strlen(buf));
+//	Rio_readlineb(&rio, buf, MAXLINE);
+//	Fputs(buf, stdout);
+//    }
     Close(clientfd); //line:netp:echoclient:close
     exit(0);
 }
--- echoserveri.c
+++ echoserveri.c
@@ -9,72 +9,23 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-char** str_split(char* a_str, const char a_delim)
-{
-    char** result    = 0;
-    size_t count     = 0;
-    char* tmp        = a_str;
-    char* last_comma = 0;
-    char delim[2];
-    delim[0] = a_delim;
-    delim[1] = 0;
-
-    /* Count how many elements will be extracted. */
-    while (*tmp)
-    {
-        if (a_delim == *tmp)
-        {
-            count++;
-            last_comma = tmp;
-        }
-        tmp++;
-    }
-
-    /* Add space for trailing token. */
-    count += last_comma < (a_str + strlen(a_str) - 1);
-
-    /* Add space for terminating null string so caller
-       knows where the list of returned strings ends. */
-    count++;
-
-    result = malloc(sizeof(char*) * count);
-
-    if (result)
-    {
-        size_t idx  = 0;
-        char* token = strtok(a_str, delim);
-
-        while (token)
-        {
-            assert(idx < count);
-            *(result + idx++) = strdup(token);
-            token = strtok(0, delim);
-        }
-        assert(idx == count - 1);
-        *(result + idx) = 0;
-    }
-
-    return result;
-}
-
-
-
 
 void echo(int connfd);
 
 int main(int argc, char **argv) 
 {
-    int listenfd, connfd, port;
+    int listenfd, connfd, port, secret_key;
     
     socklen_t clientlen;
     struct sockaddr_in clientaddr;
     struct hostent *hp;
     char *haddrp;
-    if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    if (argc < 3) {
+	fprintf(stderr, "usage: %s <port> <secret>\n", argv[0]);
 	exit(0);
     }
     port = atoi(argv[1]);
+    secret_key = atoi(argv[2]);
 
     listenfd = Open_listenfd(port);
     while (1) {
@@ -88,13 +39,14 @@ int main(int argc, char **argv)
 	printf("server connected to %s (%s)\n", hp->h_name, haddrp);
 
 
-	char buf[MAXLINE];
-	rio_t rio;
-	Rio_readinitb(&rio, connfd);
-	Rio_readlineb(&rio, buf, MAXLINE);
-
+//	char buf[1024*100];
+//	rio_t rio;
+//	Rio_readinitb(&rio, connfd);
+//	Rio_readn(&rio, buf, 100*1024);
+/*
 	char **tokens;
 	tokens = str_split(buf, ' ');
+
 	if(strcmp("mcput", tokens[0]) == 0)
 	{
 	  printf("Request Type = put\n");
@@ -116,6 +68,7 @@ int main(int argc, char **argv)
 	  printf("Request Type = list\n");
 	}
 	//echo(connfd);
+*/
 	Close(connfd);
     }
     exit(0);
