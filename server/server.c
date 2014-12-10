b7f3bed90d4bc5f3ec40810ac77bfec533ac4a32
ELepianka <errol.lepianka@uky.edu>
2014-12-10 18:18:17 -0500

    changed server

--- server/server.c
+++ server/server.c
@@ -1,7 +1,3 @@
-/* 
- * echoserveri.c - An iterative echo server 
- */ 
-/* $begin echoserverimain */
 #include "../include/csapp.h"
 #include "../include/options.h"
 #include <stdlib.h>
@@ -9,74 +5,54 @@
 #include <assert.h>
 #include <string.h>
 
-void echo(int connfd);
+int main(int argc, char **argv) {
 
-int main(int argc, char **argv) 
-{
-    int listenfd, connfd, port, secret_key, client_key;
+    int listenfd, connfd, port, secret_key; 
     
     socklen_t clientlen;
     struct sockaddr_in clientaddr;
     struct hostent *hp;
     char *haddrp;
     if (argc < 3) {
-	fprintf(stderr, "usage: %s <port> <secret>\n", argv[0]);
-	exit(0);
-    }
+    fprintf(stderr, "usage: %s <port> <secret>\n", argv[0]);
+    exit(0);
+    }    
     port = atoi(argv[1]);
     secret_key = atoi(argv[2]);
 
     listenfd = Open_listenfd(port);
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    	clientlen = sizeof(clientaddr);
+    	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 
-	/* Determine the domain name and IP address of the client */
-	hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-			   sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-	haddrp = inet_ntoa(clientaddr.sin_addr);
-	printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+        /* Determine the domain name and IP address of the client */
+        hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+        sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        haddrp = inet_ntoa(clientaddr.sin_addr);
+        printf("server connected to %s (%s)\n", hp->h_name, haddrp);
 
+    	char* buf;
+        size_t size;
+    	size = sizeof(char)*(CONTENT_MAX);
+    	rio_t rio;
+//    	Rio_readinitb(&rio, connfd);
+        char* tmp;
+        int user_key = 0;
+        int tmpint = 0;       
+ 
+        printf("did not read anything!");
+        read(&rio, tmp, 4);
+        user_key = atoi(tmp);
+        printf("read something!\n%d\n",user_key);
+        if (user_key != secret_key){
+            return -1;
+        }
+//        Rio_readnb(&rio, tmp, 4);
+//        tmpint = atoi(tmp);
+//        if (tmpint != 1){
+//           return -1;
+//        }       
 
-
-
-
-//	char buf[1024*100];
-	char* buf;
-	size_t size;
-	size = sizeof(char)*(100*1024);
-	rio_t rio;
-	Rio_readinitb(&rio, connfd);
-	Rio_readnb(&rio, buf, size);
-
-	char **tokens;
-//	tokens = str_split(buf, ' ');
-
-//	if(strcmp("mcput", tokens[0]) == 0)
-	{
-	  printf("Request Type = put\n");
-	  //Rio_writen(&rio, buf, size);
-	  //do put stuff
-	}
-/*	if("mcget", tokens[0])
-	{
-	  printf("Request Type = get\n");
-	  //do get stuff
-	}
-	
-	else if("mcdel", tokens[0])
-	{
-	  printf("Request Type = del\n");
-	  //
-	}
-	else if("mclist", tokens[0])
-	{
-	  printf("Request Type = list\n");
-	}
-	//echo(connfd);
-*/
-	Close(connfd);
+        exit(0);
     }
-    exit(0);
 }
-/* $end echoserverimain */
