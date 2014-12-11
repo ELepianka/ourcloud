9023645d752198cb00882a5ae28707b39efefdfe
ELepianka <errol.lepianka@uky.edu>
2014-12-11 02:45:22 -0500

    finishing up PUT

--- server/server.c
+++ server/server.c
@@ -40,13 +40,13 @@ int main(int argc, char **argv) {
     	rio_t rio;
     	Rio_readinitb(&rio, connfd);
         char* tmp = malloc(PUT_REQ_HEADER+CONTENT_MAX);
-        memset(tmp,0,PUT_REQ_HEADER+CONTENT_MAX);
+//        memset(tmp,0,PUT_REQ_HEADER+CONTENT_MAX);
         int user_key = 0;
         rio_readnb(&rio, tmp, PUT_REQ_HEADER+CONTENT_MAX);
-    	memcpy(&user_key, tmp, 4);
+    	memcpy(&user_key, tmp, 4);//user_key = bytes 0-3
         user_key = ntohl(user_key);
         if (user_key != secret_key){
-	        printf("incorrect key%d\n", user_key);
+	        printf("incorrect key\n");
             return -1;
         }
         else {
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
         }
 
 	    unsigned int request_type = 5;
-        memcpy(&request_type, tmp+4, 4);
+        memcpy(&request_type, tmp+4, 4); //type = bytes 4-7
 	    request_type = ntohl(request_type);
         if(request_type == 5){
 	        printf("Poorly formatted request\n");
@@ -62,25 +62,32 @@ int main(int argc, char **argv) {
 
         int resp = 0;
         char filename[FNAME_MAX];
-        char buf[CONTENT_MAX];
-	int size;
+//        char path[10+FNAME_MAX];
+        char* buf = malloc(CONTENT_MAX);
+        int size = 0;
         switch(request_type){
             case 0:
                 printf("Request Type = get\n");
                 break;
             case 1:
                 printf("Request Type = put\n");
-                memcpy(filename, tmp+4+4, 80);
+                memcpy(filename, tmp+4+4, 80); //filename = bytes 8-87
                 printf("Filename = %s\n",filename);
-                memcpy(&size,tmp+88, 4);          
-		size = ntohl(size);
+                memcpy(&size,tmp+88,4);
+                size = ntohl(size);
                 printf("Size of content: %d\n",size);
-//                memcpy(buf,tmp+4+4+80+4,size);
-//                
+//                strcpy(path,"../server/");
+//                printf("path = %s\n",path);
+//                strcat(path,filename);
+//                printf("path = %s\n",path);
+//                memcpy(buf,tmp+92,&size);
+//                printf("got past memcpy of content\n");
 //                FILE *fp;
 //                fp = fopen(filename,"w");
-//                fwrite(buf,sizeof(char),CONTENT_MAX,fp);
+//                printf("got past the fopen\n");
+//                fwrite(buf,sizeof(char),size,fp);
 //                fclose(fp);
+
                 Rio_writen(connfd,&resp,4);
                 break;
             case 2:
