97ae7e621471429032f9510845c60abeebc7774c
ELepianka <errol.lepianka@uky.edu>
2014-12-10 21:11:53 -0500

    working server for PUT

--- server/server.c
+++ server/server.c
@@ -5,14 +5,27 @@
 #include <assert.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
-
-    int listenfd, connfd, port, secret_key; 
-    
-    socklen_t clientlen;
-    struct sockaddr_in clientaddr;
+void connection(socklen_t clientlen, struct sockaddr_in clientaddr){
     struct hostent *hp;
     char *haddrp;
+    
+    /* Determine the domain name and IP address of the client */
+    hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+    sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    haddrp = inet_ntoa(clientaddr.sin_addr);
+    printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+}
+
+
+
+int main(int argc, char **argv) {
+
+    int listenfd, connfd, port; 
+    unsigned int secret_key;
+    socklen_t clientlen;
+    struct sockaddr_in clientaddr;
+//    struct hostent *hp;
+//    char *haddrp;
     if (argc < 3) {
     fprintf(stderr, "usage: %s <port> <secret>\n", argv[0]);
     exit(0);
@@ -25,51 +38,51 @@ int main(int argc, char **argv) {
     	clientlen = sizeof(clientaddr);
     	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 
-        /* Determine the domain name and IP address of the client */
-        hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-        sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-        haddrp = inet_ntoa(clientaddr.sin_addr);
-        printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+        connection(clientlen,clientaddr);
 
-    	char* buf;
-        size_t size;
-    	size = sizeof(char)*(CONTENT_MAX);
     	rio_t rio;
     	Rio_readinitb(&rio, connfd);
         char* tmp = malloc(PUT_REQ_HEADER+CONTENT_MAX);
+        memset(tmp,0,PUT_REQ_HEADER+CONTENT_MAX);
         int user_key = 0;
-        int tmpint = 0;       
+
         rio_readnb(&rio, tmp, PUT_REQ_HEADER+CONTENT_MAX);
-	memcpy(&user_key, tmp, 4);
+
+    	memcpy(&user_key, tmp, 4);
         user_key = ntohl(user_key);
-        printf("read something!\n%s\n",tmp);
         if (user_key != secret_key){
-	  printf("incorrect key\n");
+	        printf("incorrect key\n");
             return -1;
         }
-	int request_type = -1;
-	memcpy(&request_type, tmp+4, 4);
-	printf("request_type = %d\n", request_type);
-	if(request_type == -1)
-	{
-	  printf("Poorly formatted request\n");
-	}
-	if(request_type == 0)
-	{
-	  printf("type: GET\n");
-	}
-	if(request_type == 1)
-	{
-	  printf("type: PUT\n");
-	}
-	if(request_type == 2)
-	{
-	  printf("type: DEL\n");
-	}
-	if(request_type == 3)
-	{
-	  printf("type: LIST\n");
-	}
+        else {
+            printf("Secret Key = %d\n",user_key);
+        }
+	    unsigned int request_type = 5;
+        memcpy(&request_type, tmp+4, 4);
+	    request_type = ntohl(request_type);
+    
+        if(request_type == 5){
+	        printf("Poorly formatted request\n");
+    	}
+        int resp = 0;
+        char filename[FNAME_MAX];
+        switch(request_type){
+            case 0:
+                printf("Request Type = get\n");
+                break;
+            case 1:
+                printf("Request Type = put\n");
+                memcpy(filename, tmp+4+4, 80);
+                printf("Filename = %s\n",filename);
+                Rio_writen(connfd,&resp,4);
+                break;
+            case 2:
+                printf("Request Type = del\n");
+                break;
+            case 3:
+                printf("Request Type = list\n");
+                break;
+        };
 	
 
         exit(0);
